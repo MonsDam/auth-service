00945d15c6e3c363262f90afd2459d34c8607e61
Fatima Garcia <garzifati09@gmail.com>
2024-12-15 22:58:45 -0600

    Fix line error and add env.example

--- server.js
+++ server.js
@@ -5,5 +5,3 @@ const config = require('./src/config/config');
 app.listen(config.port, () => {
     console.log(`Servidor corriendo en http://localhost:${config.port}`);
 });
-
-module.exports = app;
